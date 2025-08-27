94792a24625f856a41156052f618bbf24f4a1982
zhouhj1993 <842211604@qq.com>
2025-08-27 17:46:38 +0800

    ContainerView新增useTopbar、leftBarItems、rightBarItems配置项

--- lib/widget/ContainerView.dart
+++ lib/widget/ContainerView.dart
@@ -3,27 +3,40 @@ import 'package:get/get.dart';
 import 'package:tdesign_flutter/tdesign_flutter.dart';
 
 class ContainerView extends StatelessWidget {
-  /// 标题
-  final String title;
-
-  /// 背景颜色
-  final Color? backgroundColor;
-
   /// 组件
   final Widget body;
 
+  /// 标题
+  final String title;
+
+  /// 是否启用topbar
+  final bool useTopbar;
+
+  /// 是否启用左侧返回按钮
   final bool useDefaultBack;
 
   /// 返回事件
   final VoidCallback? onBack;
 
+  /// 背景颜色
+  final Color? backgroundColor;
+
+  /// 自定义左边操作项
+  final List<TDNavBarItem>? leftBarItems;
+
+  /// 自定义右边操作项
+  final List<TDNavBarItem>? rightBarItems;
+
   const ContainerView({
     super.key,
-    required this.title,
     required this.body,
+    this.title = "",
+    this.useTopbar = true,
     this.useDefaultBack = true,
     this.onBack,
     this.backgroundColor = Colors.white,
+    this.leftBarItems,
+    this.rightBarItems,
   });
 
   @override
@@ -32,20 +45,27 @@ class ContainerView extends StatelessWidget {
       backgroundColor: backgroundColor,
       body: Column(
         children: [
-          TDNavBar(
-            leftBarItems: _buildNavBarItems(),
-            useDefaultBack: false,
-            titleColor: TDTheme.of(context).whiteColor1,
-            title: title,
-            backgroundColor: TDTheme.of(context).brandNormalColor,
-          ),
-          Expanded(child: body)
+          if (useTopbar) _buildNavBar(context),
+          Expanded(child: body),
         ],
       ),
     );
   }
 
-  List<TDNavBarItem>? _buildNavBarItems() {
+  /// 构建TopBar
+  TDNavBar _buildNavBar(BuildContext context) {
+    return TDNavBar(
+      title: title,
+      leftBarItems: _buildLeftItems(),
+      rightBarItems: rightBarItems,
+      useDefaultBack: false,
+      titleColor: TDTheme.of(context).whiteColor1,
+      backgroundColor: TDTheme.of(context).brandNormalColor,
+    );
+  }
+
+  /// 构建Topbar左侧按钮
+  List<TDNavBarItem>? _buildLeftItems() {
     final backItem = TDNavBarItem(
         icon: TDIcons.chevron_left,
         iconSize: 24,
@@ -53,6 +73,6 @@ class ContainerView extends StatelessWidget {
         action: () {
           onBack == null ? Get.back() : onBack?.call();
         });
-    return useDefaultBack ? [backItem] : null;
+    return useDefaultBack ? [backItem] : leftBarItems;
   }
 }
